cc0b68b5e08346a81f3d9f60d3bba5c3e7d78f13
Mark Stewart <markstewart@dbc-markstewart.local>
2016-03-28 14:19:19 -0500

    refactor to fewer methods in Repo class

--- lib/repos_report/repo_finder.rb
+++ lib/repos_report/repo_finder.rb
@@ -4,18 +4,7 @@ module RepoFinder
   class << self
 
     def repos_in_or_below(directory)
-      if all_child_folders_have_repos(directory) # Base case
-        repos_in(directory)
-
-      else
-        repos = repos_in(directory)
-
-        folders_without_repos_in(directory).each do |folder|
-          repos += repos_in_or_below(folder)
-        end
-
-        repos
-      end
+      repos_in(directory) + repos_in_subdirectories_of(directory)
     end
 
 
@@ -23,31 +12,27 @@ module RepoFinder
     private
 
     def repos_in(directory)
-      return [] if folders_in(directory).empty?
-
       folders_with_repos_in(directory).map { |d| Repo.new(d) }
     end
 
-    def folders_without_repos_in(directory)
-      directories = folders_in(directory)
-
-      directories.reject { |d| contains_repo?(d) }
-    end
-
     def folders_with_repos_in(directory)
-      directories = folders_in(directory)
+      directories = subdirectories_in(directory)
       directories.select { |d| contains_repo?(d) }
     end
 
-    def all_child_folders_have_repos(directory)
-      folders_with_repos_in(directory).sort == folders_in(directory).sort
+    def repos_in_subdirectories_of(directory)
+      subdirectories = subdirectories_in(directory)
+
+      subdirectories.reduce([]) do |repos, subdirectory| 
+        repos += repos_in_or_below(subdirectory)
+      end
     end
 
     def contains_repo?(directory_path)
       Dir.glob("#{directory_path}/.git").any?
     end
 
-    def folders_in(directory)
+    def subdirectories_in(directory)
       Dir.glob("#{directory}/*")
     end
   end
